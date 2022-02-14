bf017b29fd701db7ccf1aed29e05bae41db9c47e
purohitparth25 <parth.purohit@wisc.edu>
2022-02-13 18:47:48 -0600

    First commit

---
+++ Para_Trial.m
@@ -0,0 +1,22 @@
+fc = 20000;
+fnom = 100;
+fsw = 2000;
+Imax = 100;
+J = 0.2;
+LO = 2e-4;
+Ld = 2e-4;
+Lq = 2e-4;
+N = 6;
+omegam_nom = 104.7198;
+PM = 0.03;
+rpm_nom = 1000;
+Rs = 0.013;
+Tmax = 27;
+Ts = 2e-6;
+Tsc = 5e-5;
+Pmax = 2.8274e3;
+% Add a comment
+
+%Hello there
+
+
